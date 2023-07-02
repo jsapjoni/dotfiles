50ecdc4084e2cb1af43925d7ce813e0ede2b5bd3
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-07-02 12:07:04 +0200

    Refactor code

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,15 +1,105 @@
 # Tilgjengeliggjør scriptet
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
+# Checks if Scoop is installed
 if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
-  Invoke-RestMethod -Uri "get.scoop.sh" | Invoke-Expression
+  Write-Host "Could not find scoop package manager"
+  Write-Host "Installing scoop package manager"
+  
+  try {
+    Invoke-RestMethod -Uri "get.scoop.sh" | Invoke-Expression -ErrorAction Stop
+    Write-Host "Scoop installed" -ForegroundColor Green
+  }
+  
+  catch {
+    Write-Host "Could not install scoop, please verify installation script"
+    Write-Host "Aborting bootstrapping ..." -ForegroundColor Red
+    throw
+  }
+  
+  Write-Host "Checking if git is installed in scoop..."
+  if ((scoop list git).Name -like "git") {
+    Write-Host "The app git" -NoNewline
+    Write-Host $_ -ForegroundColor Green -NoNewline
+    Write-Host " is installed!" -NoNewline
+  }
+  
+  else {
+    Write-Host "The app git" -NoNewline
+    Write-Host $_ -ForegroundColor Red -NoNewline
+    Write-Host " is not installed." -NoNewline
+    Write-Host "Initiating installation..."
+    scoop install $_
+    if ((scoop list git).Name -like "git") {
+      scoop bucket add extras
+    }
+  }
+  
+  if (!(Test-Path -Path ~\.dotfiles)) {
+    git clone https://github.com/jsapjoni/dotfiles .\.dotfiles
+  }
 }
 
-# Insert App Installasjon via Package Manager
-scoop install git 
-scoop bucket add extras
-scoop install 7zip bat curl dark fd fzf gcc jq lazygit lua make neofetch neovim nvm pwsh python sudo windows-terminal
+# ------------------------- VARIABLE DECLEARATION ------------------------------#
+$ProfileType = $PROFILE.CurrentUserCurrentHost
+$ConfigFiles = "$($MyInvocation.MyCommand.Path | Split-Path)\windows\powershell\config"
+# ------------------------------------------------------------------------------#
 
-git clone https://github.com/jsapjoni/dotfiles .
+$AppsList = @(
+  "git",
+  "pwsh",
+  "7zip",
+  "bat",
+  "curl",
+  "dark",
+  "fd",
+  "fzf",
+  "gcc",
+  "jq",
+  "lua",
+  "make",
+  "neofetch",
+  "neovim",
+  "nvm",
+  "python",
+  "sudo",
+  "ripgrep",
+  "windows-terminal",
+  "lazygit"
+)
 
-# Insert Symlinks
+$AppsList | ForEach-Object {
+  if ((scoop list $_).Name -like $_) {
+    Write-Host "The app: " -NoNewline
+    Write-Host $_ -ForegroundColor Green -NoNewline
+    Write-Host " is installed!" -NoNewline
+  }
+  
+  else {
+    Write-Host "The app: " -NoNewline
+    Write-Host $_ -ForegroundColor Red -NoNewline
+    Write-Host " is not installed." -NoNewline
+    Write-Host "Initiating installation..."
+    scoop install $_
+  }
+}
+
+Write-Host "Bootstrapping powershell profile from dotfiles"
+if (!(Test-Path -Path $ProfileType)){
+  Write-Host "Importing profile..."
+  try {
+    $CopyItemArgs = @{
+      "Path" = "$ConfigFiles\PSProfile.ps1"
+      "Destination" = $ProfileType
+    }
+    Copy-Item @CopyItemArgs -ErrorAction Stop
+    Write-Host "Profile import complete!" -ForegroundColor Green
+  }
+  catch {
+    Write-Host "Could not import profile" -ForegroundColor Red
+    throw
+  }
+} 
+else { 
+  Write-Host "Profile already exists" -ForegroundColor Green
+}

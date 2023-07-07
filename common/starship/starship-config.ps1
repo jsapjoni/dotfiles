73455de3843ba2a8b392511ac79a3114a12e1c05
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-07-07 22:03:34 +0200

    Refactor code

--- common/starship/starship-config.ps1
+++ common/starship/starship-config.ps1
@@ -5,10 +5,21 @@ try {
     "Value" = $(Resolve-Path ~\.dotfiles\common\starship\starship.toml)
     "ErrorAction" = "Stop"
   } ; New-Item @NewItemSymbolicLinkSplash | Out-Null 
+
 }
 catch [System.IO.IOException] {
   Write-Host "SymbolicLink already exists"
 }
 catch {
   Write-Host "Could not create new SymbolicLink"
+}
+
+try {
+  if (!(Get-Content -Path $PROFILE.CurrentUserCurrentHost |
+    Select-String -Pattern "Invoke-Expression \(&starship init powershell\)") -is [System.Object]) {
+    "Invoke-Expression \(&starship init powershell\)" >> $PROFILE.CurrentUserCurrentHost
+  }
+}
+catch {
+  Write-Host "Could not write configuration to powershell profile"
 }

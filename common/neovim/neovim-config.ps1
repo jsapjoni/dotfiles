b32fe3a8a52d4594f13c3be338e26ae655c5078b
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-07-07 14:03:41 +0200

    Refactor code

--- common/neovim/neovim-config.ps1
+++ common/neovim/neovim-config.ps1
@@ -1,6 +1,13 @@
-if (!(Test-Path -Path "$env:LOCALAPPDATA\nvim")) {
-  New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Value $(Resolve-Path ~\.dotfiles\common\neovim\neovim) -ErrorAction SilentlyContinue | Out-Null 
-} else { Write-Host "SymbolicLink already established"}
+#if (!(Test-Path -Path "$env:LOCALAPPDATA\nvim")) {
+#  New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Value $(Resolve-Path ~\.dotfiles\common\neovim\neovim) -ErrorAction SilentlyContinue | Out-Null 
+#} else { Write-Host "SymbolicLink already established"}
 
-
-
+try {
+  New-Item -ItemType SymbolicLink -Path "$env:LOCALAPPDATA\nvim" -Value $(Resolve-Path ~\.dotfiles\common\neovim\neovim) | Out-Null 
+}
+catch [System.IO.IOException] {
+  Write-Host "SymbolicLink already exists"
+}
+catch {
+  Write-Host "Could not create new SymbolicLink"
+}

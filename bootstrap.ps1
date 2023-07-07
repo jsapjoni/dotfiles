5ed5cf84c3a714922a074fe81966fc3e41cab538
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-07-07 07:10:07 +0200

    Refactor code

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -72,13 +72,13 @@ if (!(Test-Path -Path "$env:USERPROFILE\.dotfiles")) {
   New-Item -Path "$env:USERPROFILE" -Name ".dotfiles" -ItemType Directory | Out-Null
   Write-Host "Cloning remote repository from URL: " -NoNewline
   Write-Host "https://github.com/jsapjoni/dotfiles" -ForegroundColor Green
-  git clone https://github.com/jsapjoni/dotfiles ~\.dotfiles
+  git clone https://github.com/jsapjoni/dotfiles $("$HOME\.dotfiles")
   if ($LASTEXITCODE -ne 0) {
     Write-Host "Could not clone remote repository, aborting script."
     throw 
   }
   Write-Host ".dotfiles repository successfully build on path " -NoNewline
-  Write-Host "$(~\.dotfiles)" -ForegroundColor Green
+  Write-Host $("$HOME\.dotfiles") -ForegroundColor Green
 } 
 else {
   Write-Host "Dotfiles is already built!"

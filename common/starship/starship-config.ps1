f4b213c2391b160af46b262633be4e342d3b85ee
Joshua Saputil <joshuasaputil97@hotmail.com>
2023-07-07 13:29:09 +0200

    Refactor code

--- common/starship/starship-config.ps1
+++ common/starship/starship-config.ps1
@@ -1,1 +1,1 @@
-New-Item -Path "~\.config\starship.toml" -ItemType SymbolicLink -Value $(Resolve-Path ~\.dotfiles\common\starship\starship.toml)
+New-Item -Path "~\.config\starship.toml" -ItemType SymbolicLink -Value $(Resolve-Path ~\.dotfiles\common\starship\starship.toml) | Out-Null
